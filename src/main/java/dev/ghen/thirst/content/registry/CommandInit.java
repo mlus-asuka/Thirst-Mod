85245d1ccee186d79dfc3ab5d7de7f09d03fdf34
mlus <1319237806@qq.com>
2024-04-23 19:35:41 +0800

    fixed #119

--- src/main/java/dev/ghen/thirst/content/registry/CommandInit.java
+++ src/main/java/dev/ghen/thirst/content/registry/CommandInit.java
@@ -11,14 +11,17 @@ import dev.ghen.thirst.foundation.network.message.PlayerThirstSyncMessage;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.PacketDistributor;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Mod.EventBusSubscriber(modid = Thirst.ID)
 public class CommandInit {
@@ -30,7 +33,7 @@ public class CommandInit {
                 .requires(cs->cs.hasPermission(2))
                 .then(Commands.literal("query").then(Commands.argument("Player", EntityArgument.player())
                         .executes(context -> {
-                                    Player player = EntityArgument.getPlayer(context,"Player");
+                                    ServerPlayer player = EntityArgument.getPlayer(context,"Player");
                                     IThirst iThirst = player.getCapability(ModCapabilities.PLAYER_THIRST).orElse(null);
                                     Object[] arg =new Object[2];
                                     arg[0]=iThirst.getThirst();
@@ -43,7 +46,7 @@ public class CommandInit {
                         .then(Commands.argument("thirst", IntegerArgumentType.integer(0,20))
                                 .then(Commands.argument("quenched", IntegerArgumentType.integer(0,20))
                                         .executes(context -> {
-                                            Player player = EntityArgument.getPlayer(context,"Player");
+                                            ServerPlayer player = EntityArgument.getPlayer(context,"Player");
                                             IThirst iThirst = player.getCapability(ModCapabilities.PLAYER_THIRST).orElse(null);
                                             Object[] arg =new Object[2];
                                             arg[0]= IntegerArgumentType.getInteger(context,"thirst");
@@ -55,20 +58,26 @@ public class CommandInit {
                                             return 0;
                                         })))
                 ))
-                .then(Commands.literal("enable").then(Commands.argument("Player",EntityArgument.player())
+                .then(Commands.literal("enable").then(Commands.argument("Player",EntityArgument.players())
                         .then(Commands.argument("bool", BoolArgumentType.bool())
                                 .executes(context ->{
-                                    ServerPlayer player = EntityArgument.getPlayer(context,"Player");
+                                    Collection<ServerPlayer> players = EntityArgument.getPlayers(context,"Player");
                                     boolean shouldTick = BoolArgumentType.getBool(context,"bool");
-                                    IThirst thirstData =  player.getCapability(ModCapabilities.PLAYER_THIRST).orElse(null);
-                                    thirstData.setShouldTickThirst(shouldTick);
-                                    ThirstModPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player),
-                                            new PlayerThirstSyncMessage(shouldTick));
-                                    if(shouldTick){
-                                        context.getSource().sendSuccess(()->MutableComponent.create(new TranslatableContents("command.thirst.enable","command.thirst.enable",new Object[]{player.getName()})),false);
-                                    }else {
-                                        context.getSource().sendSuccess(()->MutableComponent.create(new TranslatableContents("command.thirst.disable","command.thirst.disable",new Object[]{player.getName()})),false);
+                                    Collection<Component> playersName = new ArrayList<>();
+                                    for(ServerPlayer player:players){
+                                        IThirst thirstData =  player.getCapability(ModCapabilities.PLAYER_THIRST).orElse(null);
+                                        thirstData.setShouldTickThirst(shouldTick);
+                                        ThirstModPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player),
+                                                new PlayerThirstSyncMessage(shouldTick));
+                                        playersName.add(player.getName());
                                     }
+
+                                    if(shouldTick){
+                                        context.getSource().sendSuccess(()->MutableComponent.create(new TranslatableContents("command.thirst.enable","command.thirst.enable",playersName.toArray())),false);
+                                    }else {
+                                        context.getSource().sendSuccess(()->MutableComponent.create(new TranslatableContents("command.thirst.disable","command.thirst.disable",playersName.toArray())),false);
+                                    }
+
                                     return 0;
                                 }))))
         );
