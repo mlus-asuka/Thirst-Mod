49823bf625d944a39e7c5f10e7419220826a2a7b
Eyeofflame643 <138283224+eyeofflame@users.noreply.github.com>
2026-01-25 14:42:45 +0800

    Delete java directory

--- java/dev/ghen/thirst/canteen/common/item/Drinkable.java
+++
@@ -1,9 +0,0 @@
-package dev.ghen.thirst.canteen.common.item;
-
-import net.minecraft.world.item.ItemStack;
-
-public interface Drinkable {
-    int getMaxUsableTimes();
-
-    int getLeftUsableTimes(ItemStack stack);
-}
